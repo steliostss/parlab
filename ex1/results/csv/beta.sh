8cd4091c15ce9746fd2cdbc47c685e2a7d96983e
Stelios <tsagarakis.stelios@gmail.com>
2019-11-27 13:10:21 +0200

    Add diagrams for report

---
+++ ex1/results/csv/beta.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for i in 2048 1024 0512 0256 00128 0064
+do
+	touch "Beta""$i".csv
+done
---
+++ ex1/results/csv/thread.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for i in 01 02 04 08 16 32 64
+do
+	touch "$i""thread.csv"
+done
