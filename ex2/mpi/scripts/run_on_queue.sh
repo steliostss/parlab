f97a172ccfa0e44cbdaba8d24c1dbbe70148e494
Stelios <tsagarakis.stelios@gmail.com>
2019-12-11 23:58:34 +0200

    Add all scenarios to run

--- ex2/mpi/scripts/run_on_queue.sh
+++ ex2/mpi/scripts/run_on_queue.sh
@@ -20,85 +20,197 @@
 module load openmpi/1.8.3
 cd /home/parallel/parlab30/a2/mpi/
 
+echo "-------------------------------------------------------------------------------------------------------------------------------------"
+echo "--------------------------------------------------JACOBI-----------------------------------------------------------------------------"
+echo "-------------------------------------------------------------------------------------------------------------------------------------"
+
+
 echo "*********************WITH CONVERGENCE**************************"
-echo "Procs 64 Grid 8X8 Array 1024"
+echo "Jabobi Procs 64 Grid 8X8 Array 1024"
 mpirun -np 64 --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 ./jacobi_mpi_conv 1024 1024 8 8
-## mpirun -np 64 --mca shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_conv 1024 1024 8 8 
 echo
 
-echo "Procs 64 Grid 4X16 Array 1024"
+echo "Jacobi Procs 64 Grid 4X16 Array 1024"
 mpirun -np 64 --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 ./jacobi_mpi_conv 1024 1024 4 16
-## mpirun -np 64 --mca shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_conv 1024 1024 4 16 
 echo
 
-echo "Procs 64 Grid 2X32 Array 1024"
+echo "Jacobi Procs 64 Grid 2X32 Array 1024"
 mpirun -np 64 --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 ./jacobi_mpi_conv 1024 1024 2 32
-## mpirun -np 64 --mca shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_conv 1024 1024 2 32
 echo
 
 echo "*********************WITHOUT CONVERGENCE**************************"
-echo "Procs 1 Array 2048"
+echo "Jacobi Procs 1 Array 2048"
 mpirun -np 1 --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 ./jacobi_mpi_NO_conv 2048 2048 1 1
-## mpirun -np 1 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_NO_conv 2048 2048 1 1
-echo "Procs 2 Array 2048"
+echo "Jacobi Procs 2 Array 2048"
 mpirun -np 2 --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 ./jacobi_mpi_NO_conv 2048 2048 1 2
-## mpirun -np 2 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_NO_conv 2048 2048 1 2
-echo "Procs 4 Array 2048"
+echo "Jacobi Procs 4 Array 2048"
 mpirun -np 4 --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 ./jacobi_mpi_NO_conv 2048 2048 2 2
-## mpirun -np 4 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_NO_conv 2048 2048 2 2
-echo "Procs 8 Array 2048"
+echo "Jacobi Procs 8 Array 2048"
 mpirun -np 8 --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 ./jacobi_mpi_NO_conv 2048 2048 2 4
-## mpirun -np 8 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_NO_conv 2048 2048 2 4
-echo "Procs 16 Array 2048"
+echo "Jacobi Procs 16 Array 2048"
 mpirun -np 16 --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 ./jacobi_mpi_NO_conv 2048 2048 4 4
-## mpirun -np 16 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_NO_conv 2048 2048 4 4
-echo "Procs 32 Array 2048"
+echo "Jacobi Procs 32 Array 2048"
 mpirun -np 32 --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 ./jacobi_mpi_NO_conv 2048 2048 4 8
-## mpirun -np 32 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_NO_conv 2048 2048 4 8
-echo "Procs 64 Array 2048"
+echo "Jacobi Procs 64 Array 2048"
 mpirun -np 64 --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 ./jacobi_mpi_NO_conv 2048 2048 8 8
-## mpirun -np 64 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_NO_conv 2048 2048 8 8
 
-echo "Procs 1 Array 4096"
+echo "Jacobi Procs 1 Array 4096"
 mpirun -np 1 --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 ./jacobi_mpi_NO_conv 4096 4096 1 1
-## mpirun -np 1 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_NO_conv 4096 4096 1 1
-echo "Procs 2 Array 4096"
+echo "Jacobi Procs 2 Array 4096"
 mpirun -np 2 --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 ./jacobi_mpi_NO_conv 4096 4096 1 2
-## mpirun -np 2 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_NO_conv 4096 4096 1 2
-echo "Procs 4 Array 4096"
+echo "Jacobi Procs 4 Array 4096"
 mpirun -np 4 --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 ./jacobi_mpi_NO_conv 4096 4096 2 2
-## mpirun -np 4 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_NO_conv 4096 4096 2 2
-echo "Procs 8 Array 4096"
+echo "Jacobi Procs 8 Array 4096"
 mpirun -np 8 --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 ./jacobi_mpi_NO_conv 4096 4096 2 4
-## mpirun -np 8 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_NO_conv 4096 4096 2 4
-echo "Procs 16 Array 4096"
+echo "Jacobi Procs 16 Array 4096"
 mpirun -np 16 --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 ./jacobi_mpi_NO_conv 4096 4096 4 4
-## mpirun -np 16 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_NO_conv 4096 4096 4 4
-echo "Procs 32 Array 4096"
+echo "Jacobi Procs 32 Array 4096"
 mpirun -np 32 --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 ./jacobi_mpi_NO_conv 4096 4096 4 8
-## mpirun -np 32 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_NO_conv 4096 4096 4 8
-echo "Procs 64 Array 4096"
+echo "Jacobi Procs 64 Array 4096"
 mpirun -np 64 --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 ./jacobi_mpi_NO_conv 4096 4096 8 8
-## mpirun -np 64 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_NO_conv 4096 4096 8 8
 
-echo "Procs 1 Array 6144"
+echo "Jacobi Procs 1 Array 6144"
 mpirun -np 1 --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 ./jacobi_mpi_NO_conv 6144 6144 1 1
-## mpirun -np 1 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_NO_conv 6144 6144 1 1
-echo "Procs 2 Array 6144"
+echo "Jacobi Procs 2 Array 6144"
 mpirun -np 2 --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 ./jacobi_mpi_NO_conv 6144 6144 1 2
-## mpirun -np 2 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_NO_conv 6144 6144 1 2
-echo "Procs 4 Array 6144"
+echo "Jacobi Procs 4 Array 6144"
 mpirun -np 4 --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 ./jacobi_mpi_NO_conv 6144 6144 2 2
-## mpirun -np 4 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_NO_conv 6144 6144 2 2
-echo "Procs 8 Array 6144"
+echo "Jacobi Procs 8 Array 6144"
 mpirun -np 8 --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 ./jacobi_mpi_NO_conv 6144 6144 2 4
-## mpirun -np 8 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_NO_conv 6144 6144 2 4
-echo "Procs 16 Array 6144"
+echo "Jacobi Procs 16 Array 6144"
 mpirun -np 16 --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 ./jacobi_mpi_NO_conv 6144 6144 4 4
-## mpirun -np 16 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_NO_conv 6144 6144 4 4
-echo "Procs 32 Array 6144"
+echo "Jacobi Procs 32 Array 6144"
 mpirun -np 32 --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 ./jacobi_mpi_NO_conv 6144 6144 4 8
-## mpirun -np 32 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_NO_conv 6144 6144 4 8
-echo "Procs 64 Array 6144"
+echo "Jacobi Procs 64 Array 6144"
 mpirun -np 64 --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 ./jacobi_mpi_NO_conv 6144 6144 8 8
-## mpirun -np 64 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_NO_conv 6144 6144 8 8
+
+
+echo "-------------------------------------------------------------------------------------------------------------------------------------"
+echo "--------------------------------------------------GAUSS-SEIDEL-----------------------------------------------------------------------"
+echo "-------------------------------------------------------------------------------------------------------------------------------------"
+
+
+echo "*********************WITH CONVERGENCE**************************"
+echo "Gauss-Seidel Procs 64 Grid 8X8 Array 1024"
+mpirun -np 64 --mca shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_mpi_conv 1024 1024 8 8 
+echo
+
+echo "Gauss-Seidel Procs 64 Grid 4X16 Array 1024"
+mpirun -np 64 --mca shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_mpi_conv 1024 1024 4 16 
+echo
+
+echo "Gauss-Seidel Procs 64 Grid 2X32 Array 1024"
+mpirun -np 64 --mca shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_mpi_conv 1024 1024 2 32
+echo
+
+echo "*********************WITHOUT CONVERGENCE**************************"
+echo "Gauss-Seidel Procs 1 Array 2048"
+mpirun -np 1 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_mpi_NO_conv 2048 2048 1 1
+echo "Gauss-Seidel Procs 2 Array 2048"
+mpirun -np 2 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_mpi_NO_conv 2048 2048 1 2
+echo "Gauss-Seidel Procs 4 Array 2048"
+mpirun -np 4 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_mpi_NO_conv 2048 2048 2 2
+echo "Gauss-Seidel Procs 8 Array 2048"
+mpirun -np 8 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_mpi_NO_conv 2048 2048 2 4
+echo "Gauss-Seidel Procs 16 Array 2048"
+mpirun -np 16 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_mpi_NO_conv 2048 2048 4 4
+echo "Gauss-Seidel Procs 32 Array 2048"
+mpirun -np 32 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_mpi_NO_conv 2048 2048 4 8
+echo "Gauss-Seidel Procs 64 Array 2048"
+mpirun -np 64 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_mpi_NO_conv 2048 2048 8 8
+
+echo "Gauss-Seidel Procs 1 Array 4096"
+mpirun -np 1 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_mpi_NO_conv 4096 4096 1 1
+echo "Gauss-Seidel Procs 2 Array 4096"
+mpirun -np 2 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_mpi_NO_conv 4096 4096 1 2
+echo "Gauss-Seidel Procs 4 Array 4096"
+mpirun -np 4 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_mpi_NO_conv 4096 4096 2 2
+echo "Gauss-Seidel Procs 8 Array 4096"
+mpirun -np 8 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_mpi_NO_conv 4096 4096 2 4
+echo "Gauss-Seidel Procs 16 Array 4096"
+mpirun -np 16 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_mpi_NO_conv 4096 4096 4 4
+echo "Gauss-Seidel Procs 32 Array 4096"
+mpirun -np 32 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_mpi_NO_conv 4096 4096 4 8
+echo "Gauss-Seidel Procs 64 Array 4096"
+mpirun -np 64 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_mpi_NO_conv 4096 4096 8 8
+
+echo "Gauss-Seidel Procs 1 Array 6144"
+mpirun -np 1 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_mpi_NO_conv 6144 6144 1 1
+echo "Gauss-Seidel Procs 2 Array 6144"
+mpirun -np 2 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_mpi_NO_conv 6144 6144 1 2
+echo "Gauss-Seidel Procs 4 Array 6144"
+mpirun -np 4 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_mpi_NO_conv 6144 6144 2 2
+echo "Gauss-Seidel Procs 8 Array 6144"
+mpirun -np 8 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_mpi_NO_conv 6144 6144 2 4
+echo "Gauss-Seidel Procs 16 Array 6144"
+mpirun -np 16 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_mpi_NO_conv 6144 6144 4 4
+echo "Gauss-Seidel Procs 32 Array 6144"
+mpirun -np 32 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_mpi_NO_conv 6144 6144 4 8
+echo "Gauss-Seidel Procs 64 Array 6144"
+mpirun -np 64 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./gauss_seidel_mpi_NO_conv 6144 6144 8 8
+
+
+echo "-------------------------------------------------------------------------------------------------------------------------------------"
+echo "--------------------------------------------------RED_BLACK_SOR----------------------------------------------------------------------"
+echo "-------------------------------------------------------------------------------------------------------------------------------------"
+
+
+
+echo "*********************WITH CONVERGENCE**************************"
+echo "Red-Black-SOR Procs 64 Grid 8X8 Array 1024"
+mpirun -np 64 --mca shmem_mmap_enable_nfs_warning 0 ./red_black_mpi_conv 1024 1024 8 8 
+echo
+
+echo "Red-Black-SOR Procs 64 Grid 4X16 Array 1024"
+mpirun -np 64 --mca shmem_mmap_enable_nfs_warning 0 ./red_black_mpi_conv 1024 1024 4 16 
+echo
+
+echo "Red-Black-SOR Procs 64 Grid 2X32 Array 1024"
+mpirun -np 64 --mca shmem_mmap_enable_nfs_warning 0 ./red_black_mpi_conv 1024 1024 2 32
+echo
+
+echo "*********************WITHOUT CONVERGENCE**************************"
+echo "Red-Black-SOR Procs 1 Array 2048"
+mpirun -np 1 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./red_black_mpi_NO_conv 2048 2048 1 1
+echo "Red-Black-SOR Procs 2 Array 2048"
+mpirun -np 2 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./red_black_mpi_NO_conv 2048 2048 1 2
+echo "Red-Black-SOR Procs 4 Array 2048"
+mpirun -np 4 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./red_black_mpi_NO_conv 2048 2048 2 2
+echo "Red-Black-SOR Procs 8 Array 2048"
+mpirun -np 8 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./red_black_mpi_NO_conv 2048 2048 2 4
+echo "Red-Black-SOR Procs 16 Array 2048"
+mpirun -np 16 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./red_black_mpi_NO_conv 2048 2048 4 4
+echo "Red-Black-SOR Procs 32 Array 2048"
+mpirun -np 32 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./red_black_mpi_NO_conv 2048 2048 4 8
+echo "Red-Black-SOR Procs 64 Array 2048"
+mpirun -np 64 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./red_black_mpi_NO_conv 2048 2048 8 8
+
+echo "Red-Black-SOR Procs 1 Array 4096"
+mpirun -np 1 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./red_black_mpi_NO_conv 4096 4096 1 1
+echo "Red-Black-SOR Procs 2 Array 4096"
+mpirun -np 2 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./red_black_mpi_NO_conv 4096 4096 1 2
+echo "Red-Black-SOR Procs 4 Array 4096"
+mpirun -np 4 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./red_black_mpi_NO_conv 4096 4096 2 2
+echo "Red-Black-SOR Procs 8 Array 4096"
+mpirun -np 8 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./red_black_mpi_NO_conv 4096 4096 2 4
+echo "Red-Black-SOR Procs 16 Array 4096"
+mpirun -np 16 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./red_black_mpi_NO_conv 4096 4096 4 4
+echo "Red-Black-SOR Procs 32 Array 4096"
+mpirun -np 32 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./red_black_mpi_NO_conv 4096 4096 4 8
+echo "Red-Black-SOR Procs 64 Array 4096"
+mpirun -np 64 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./red_black_mpi_NO_conv 4096 4096 8 8
+
+echo "Red-Black-SOR Procs 1 Array 6144"
+mpirun -np 1 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./red_black_mpi_NO_conv 6144 6144 1 1
+echo "Red-Black-SOR Procs 2 Array 6144"
+mpirun -np 2 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./red_black_mpi_NO_conv 6144 6144 1 2
+echo "Red-Black-SOR Procs 4 Array 6144"
+mpirun -np 4 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./red_black_mpi_NO_conv 6144 6144 2 2
+echo "Red-Black-SOR Procs 8 Array 6144"
+mpirun -np 8 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./red_black_mpi_NO_conv 6144 6144 2 4
+echo "Red-Black-SOR Procs 16 Array 6144"
+mpirun -np 16 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./red_black_mpi_NO_conv 6144 6144 4 4
+echo "Red-Black-SOR Procs 32 Array 6144"
+mpirun -np 32 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./red_black_mpi_NO_conv 6144 6144 4 8
+echo "Red-Black-SOR Procs 64 Array 6144"
+mpirun -np 64 --map-by node --mca btl tcp,self shmem_mmap_enable_nfs_warning 0 ./red_black_mpi_NO_conv 6144 6144 8 8
