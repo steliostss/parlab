69f634d14095acfa2bf90c4f5161358cd7a953ca
Stelios <tsagarakis.stelios@gmail.com>
2019-12-12 13:43:31 +0200

    Fix flags for shell script

--- ex2/mpi/scripts/mpirun_on_queue.sh
+++ ex2/mpi/scripts/mpirun_on_queue.sh
@@ -20,6 +20,8 @@
 module load openmpi/1.8.3
 cd /home/parallel/parlab30/a2/mpi/
 
+MPIFLAGS="--map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0"
+
 echo "-------------------------------------------------------------------------------------------------------------------------------------"
 echo "--------------------------------------------------JACOBI-----------------------------------------------------------------------------"
 echo "-------------------------------------------------------------------------------------------------------------------------------------"
@@ -27,37 +29,37 @@ echo
 echo
 
 echo "*********************WITH CONVERGENCE**************************"
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 64 ./jacobi_mpi_conv 1024 1024 8 8
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 64 ./jacobi_mpi_conv 1024 1024 4 16
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 64 ./jacobi_mpi_conv 1024 1024 2 32
+mpirun $MPIFLAGS -np 64 ./jacobi_mpi_conv 1024 1024 8 8
+mpirun $MPIFLAGS -np 64 ./jacobi_mpi_conv 1024 1024 4 16
+mpirun $MPIFLAGS -np 64 ./jacobi_mpi_conv 1024 1024 2 32
 echo
 echo
 
 echo "*********************WITHOUT CONVERGENCE**************************"
 
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 1 ./jacobi_mpi_NO_conv 2048 2048 1 1
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 2 ./jacobi_mpi_NO_conv 2048 2048 1 2
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 4 ./jacobi_mpi_NO_conv 2048 2048 2 2
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 8 ./jacobi_mpi_NO_conv 2048 2048 2 4
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 16 ./jacobi_mpi_NO_conv 2048 2048 4 4
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 32 ./jacobi_mpi_NO_conv 2048 2048 4 8
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 64 ./jacobi_mpi_NO_conv 2048 2048 8 8
+mpirun $MPIFLAGS -np 1 ./jacobi_mpi_NO_conv 2048 2048 1 1
+mpirun $MPIFLAGS -np 2 ./jacobi_mpi_NO_conv 2048 2048 1 2
+mpirun $MPIFLAGS -np 4 ./jacobi_mpi_NO_conv 2048 2048 2 2
+mpirun $MPIFLAGS -np 8 ./jacobi_mpi_NO_conv 2048 2048 2 4
+mpirun $MPIFLAGS -np 16 ./jacobi_mpi_NO_conv 2048 2048 4 4
+mpirun $MPIFLAGS -np 32 ./jacobi_mpi_NO_conv 2048 2048 4 8
+mpirun $MPIFLAGS -np 64 ./jacobi_mpi_NO_conv 2048 2048 8 8
 
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 1 ./jacobi_mpi_NO_conv 4096 4096 1 1
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 2 ./jacobi_mpi_NO_conv 4096 4096 1 2
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 4 ./jacobi_mpi_NO_conv 4096 4096 2 2
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 8 ./jacobi_mpi_NO_conv 4096 4096 2 4
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 16 ./jacobi_mpi_NO_conv 4096 4096 4 4
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 32 ./jacobi_mpi_NO_conv 4096 4096 4 8
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 64 ./jacobi_mpi_NO_conv 4096 4096 8 8
+mpirun $MPIFLAGS -np 1 ./jacobi_mpi_NO_conv 4096 4096 1 1
+mpirun $MPIFLAGS -np 2 ./jacobi_mpi_NO_conv 4096 4096 1 2
+mpirun $MPIFLAGS -np 4 ./jacobi_mpi_NO_conv 4096 4096 2 2
+mpirun $MPIFLAGS -np 8 ./jacobi_mpi_NO_conv 4096 4096 2 4
+mpirun $MPIFLAGS -np 16 ./jacobi_mpi_NO_conv 4096 4096 4 4
+mpirun $MPIFLAGS -np 32 ./jacobi_mpi_NO_conv 4096 4096 4 8
+mpirun $MPIFLAGS -np 64 ./jacobi_mpi_NO_conv 4096 4096 8 8
 
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 1 ./jacobi_mpi_NO_conv 6144 6144 1 1
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 2 ./jacobi_mpi_NO_conv 6144 6144 1 2
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 4 ./jacobi_mpi_NO_conv 6144 6144 2 2
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 8 ./jacobi_mpi_NO_conv 6144 6144 2 4
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 16 ./jacobi_mpi_NO_conv 6144 6144 4 4
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 32 ./jacobi_mpi_NO_conv 6144 6144 4 8
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 64 ./jacobi_mpi_NO_conv 6144 6144 8 8
+mpirun $MPIFLAGS -np 1 ./jacobi_mpi_NO_conv 6144 6144 1 1
+mpirun $MPIFLAGS -np 2 ./jacobi_mpi_NO_conv 6144 6144 1 2
+mpirun $MPIFLAGS -np 4 ./jacobi_mpi_NO_conv 6144 6144 2 2
+mpirun $MPIFLAGS -np 8 ./jacobi_mpi_NO_conv 6144 6144 2 4
+mpirun $MPIFLAGS -np 16 ./jacobi_mpi_NO_conv 6144 6144 4 4
+mpirun $MPIFLAGS -np 32 ./jacobi_mpi_NO_conv 6144 6144 4 8
+mpirun $MPIFLAGS -np 64 ./jacobi_mpi_NO_conv 6144 6144 8 8
 
 echo
 echo
@@ -68,35 +70,33 @@ echo
 echo
 
 echo "*********************WITH CONVERGENCE**************************"
-mpirun --mca shmem_mmap_enable_nfs_warning 0 -np 64 ./gauss_seidel_mpi_conv 1024 1024 8 8 
-mpirun --mca shmem_mmap_enable_nfs_warning 0 -np 64 ./gauss_seidel_mpi_conv 1024 1024 4 16 
-mpirun --mca shmem_mmap_enable_nfs_warning 0 -np 64 ./gauss_seidel_mpi_conv 1024 1024 2 32
-echo
-echo
-echo "*********************WITHOUT CONVERGENCE**************************"
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 1 ./gauss_seidel_mpi_NO_conv 2048 2048 1 1
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 2 ./gauss_seidel_mpi_NO_conv 2048 2048 1 2
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 4 ./gauss_seidel_mpi_NO_conv 2048 2048 2 2
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 8 ./gauss_seidel_mpi_NO_conv 2048 2048 2 4
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 16 ./gauss_seidel_mpi_NO_conv 2048 2048 4 4
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 32 ./gauss_seidel_mpi_NO_conv 2048 2048 4 8
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 64 ./gauss_seidel_mpi_NO_conv 2048 2048 8 8
+mpirun $MPIFLAGS -np 64 ./gauss_seidel_mpi_conv 1024 1024 8 8 
+mpirun $MPIFLAGS -np 64 ./gauss_seidel_mpi_conv 1024 1024 4 16 
+mpirun $MPIFLAGS -np 64 ./gauss_seidel_mpi_conv 1024 1024 2 32
 
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 1 ./gauss_seidel_mpi_NO_conv 4096 4096 1 1
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 2 ./gauss_seidel_mpi_NO_conv 4096 4096 1 2
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 4 ./gauss_seidel_mpi_NO_conv 4096 4096 2 2
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 8 ./gauss_seidel_mpi_NO_conv 4096 4096 2 4
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 16 ./gauss_seidel_mpi_NO_conv 4096 4096 4 4
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 32 ./gauss_seidel_mpi_NO_conv 4096 4096 4 8
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 64 ./gauss_seidel_mpi_NO_conv 4096 4096 8 8
+mpirun $MPINODEFLAGS -np 1 ./gauss_seidel_mpi_NO_conv 2048 2048 1 1
+mpirun $MPINODEFLAGS -np 2 ./gauss_seidel_mpi_NO_conv 2048 2048 1 2
+mpirun $MPINODEFLAGS -np 4 ./gauss_seidel_mpi_NO_conv 2048 2048 2 2
+mpirun $MPINODEFLAGS -np 8 ./gauss_seidel_mpi_NO_conv 2048 2048 2 4
+mpirun $MPINODEFLAGS -np 16 ./gauss_seidel_mpi_NO_conv 2048 2048 4 4
+mpirun $MPINODEFLAGS -np 32 ./gauss_seidel_mpi_NO_conv 2048 2048 4 8
+mpirun $MPINODEFLAGS -np 64 ./gauss_seidel_mpi_NO_conv 2048 2048 8 8
 
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 1 ./gauss_seidel_mpi_NO_conv 6144 6144 1 1
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 2 ./gauss_seidel_mpi_NO_conv 6144 6144 1 2
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 4 ./gauss_seidel_mpi_NO_conv 6144 6144 2 2
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 8 ./gauss_seidel_mpi_NO_conv 6144 6144 2 4
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 16 ./gauss_seidel_mpi_NO_conv 6144 6144 4 4
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 32 ./gauss_seidel_mpi_NO_conv 6144 6144 4 8
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 64 ./gauss_seidel_mpi_NO_conv 6144 6144 8 8
+mpirun $MPINODEFLAGS -np 1 ./gauss_seidel_mpi_NO_conv 4096 4096 1 1
+mpirun $MPINODEFLAGS -np 2 ./gauss_seidel_mpi_NO_conv 4096 4096 1 2
+mpirun $MPINODEFLAGS -np 4 ./gauss_seidel_mpi_NO_conv 4096 4096 2 2
+mpirun $MPINODEFLAGS -np 8 ./gauss_seidel_mpi_NO_conv 4096 4096 2 4
+mpirun $MPINODEFLAGS -np 16 ./gauss_seidel_mpi_NO_conv 4096 4096 4 4
+mpirun $MPINODEFLAGS -np 32 ./gauss_seidel_mpi_NO_conv 4096 4096 4 8
+mpirun $MPINODEFLAGS -np 64 ./gauss_seidel_mpi_NO_conv 4096 4096 8 8
+
+mpirun $MPINODEFLAGS -np 1 ./gauss_seidel_mpi_NO_conv 6144 6144 1 1
+mpirun $MPINODEFLAGS -np 2 ./gauss_seidel_mpi_NO_conv 6144 6144 1 2
+mpirun $MPINODEFLAGS -np 4 ./gauss_seidel_mpi_NO_conv 6144 6144 2 2
+mpirun $MPINODEFLAGS -np 8 ./gauss_seidel_mpi_NO_conv 6144 6144 2 4
+mpirun $MPIFLAGS -np 16 ./gauss_seidel_mpi_NO_conv 6144 6144 4 4
+mpirun $MPIFLAGS -np 32 ./gauss_seidel_mpi_NO_conv 6144 6144 4 8
+mpirun $MPIFLAGS -np 64 ./gauss_seidel_mpi_NO_conv 6144 6144 8 8
 
 echo
 echo
@@ -108,33 +108,33 @@ echo
 
 
 echo "*********************WITH CONVERGENCE**************************"
-mpirun --mca shmem_mmap_enable_nfs_warning 0 -np 64 ./red_black_mpi_conv 1024 1024 8 8 
-mpirun --mca shmem_mmap_enable_nfs_warning 0 -np 64 ./red_black_mpi_conv 1024 1024 4 16 
-mpirun --mca shmem_mmap_enable_nfs_warning 0 -np 64 ./red_black_mpi_conv 1024 1024 2 32
+mpirun $MPIFLAGS -np 64 ./red_black_mpi_conv 1024 1024 8 8 
+mpirun $MPIFLAGS -np 64 ./red_black_mpi_conv 1024 1024 4 16 
+mpirun $MPIFLAGS -np 64 ./red_black_mpi_conv 1024 1024 2 32
 echo
 echo
 
 echo "*********************WITHOUT CONVERGENCE**************************"
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 1 ./red_black_mpi_NO_conv 2048 2048 1 1
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 2 ./red_black_mpi_NO_conv 2048 2048 1 2
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 4 ./red_black_mpi_NO_conv 2048 2048 2 2
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 8 ./red_black_mpi_NO_conv 2048 2048 2 4
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 16 ./red_black_mpi_NO_conv 2048 2048 4 4
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 32 ./red_black_mpi_NO_conv 2048 2048 4 8
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 64 ./red_black_mpi_NO_conv 2048 2048 8 8
+mpirun $MPIFLAGS -np 1 ./red_black_mpi_NO_conv 2048 2048 1 1
+mpirun $MPIFLAGS -np 2 ./red_black_mpi_NO_conv 2048 2048 1 2
+mpirun $MPIFLAGS -np 4 ./red_black_mpi_NO_conv 2048 2048 2 2
+mpirun $MPIFLAGS -np 8 ./red_black_mpi_NO_conv 2048 2048 2 4
+mpirun $MPIFLAGS -np 16 ./red_black_mpi_NO_conv 2048 2048 4 4
+mpirun $MPIFLAGS -np 32 ./red_black_mpi_NO_conv 2048 2048 4 8
+mpirun $MPIFLAGS -np 64 ./red_black_mpi_NO_conv 2048 2048 8 8
 
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 1 ./red_black_mpi_NO_conv 4096 4096 1 1
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 2 ./red_black_mpi_NO_conv 4096 4096 1 2
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 4 ./red_black_mpi_NO_conv 4096 4096 2 2
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 8 ./red_black_mpi_NO_conv 4096 4096 2 4
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 16 ./red_black_mpi_NO_conv 4096 4096 4 4
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 32 ./red_black_mpi_NO_conv 4096 4096 4 8
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 64 ./red_black_mpi_NO_conv 4096 4096 8 8
+mpirun $MPIFLAGS -np 1 ./red_black_mpi_NO_conv 4096 4096 1 1
+mpirun $MPIFLAGS -np 2 ./red_black_mpi_NO_conv 4096 4096 1 2
+mpirun $MPIFLAGS -np 4 ./red_black_mpi_NO_conv 4096 4096 2 2
+mpirun $MPIFLAGS -np 8 ./red_black_mpi_NO_conv 4096 4096 2 4
+mpirun $MPIFLAGS -np 16 ./red_black_mpi_NO_conv 4096 4096 4 4
+mpirun $MPIFLAGS -np 32 ./red_black_mpi_NO_conv 4096 4096 4 8
+mpirun $MPIFLAGS -np 64 ./red_black_mpi_NO_conv 4096 4096 8 8
 
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 1 ./red_black_mpi_NO_conv 6144 6144 1 1
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 2 ./red_black_mpi_NO_conv 6144 6144 1 2
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 4 ./red_black_mpi_NO_conv 6144 6144 2 2
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 8 ./red_black_mpi_NO_conv 6144 6144 2 4
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 16 ./red_black_mpi_NO_conv 6144 6144 4 4
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 32 ./red_black_mpi_NO_conv 6144 6144 4 8
-mpirun --map-by node --mca btl tcp,self --mca shmem_mmap_enable_nfs_warning 0 -np 64 ./red_black_mpi_NO_conv 6144 6144 8 8
+mpirun $MPIFLAGS -np 1 ./red_black_mpi_NO_conv 6144 6144 1 1
+mpirun $MPIFLAGS -np 2 ./red_black_mpi_NO_conv 6144 6144 1 2
+mpirun $MPIFLAGS -np 4 ./red_black_mpi_NO_conv 6144 6144 2 2
+mpirun $MPIFLAGS -np 8 ./red_black_mpi_NO_conv 6144 6144 2 4
+mpirun $MPIFLAGS -np 16 ./red_black_mpi_NO_conv 6144 6144 4 4
+mpirun $MPIFLAGS -np 32 ./red_black_mpi_NO_conv 6144 6144 4 8
+mpirun $MPIFLAGS -np 64 ./red_black_mpi_NO_conv 6144 6144 8 8
