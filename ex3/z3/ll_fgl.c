e2b0836c86e1ee7faa23885e7ea822f58ac15717
Nikos Tsakas <ntsakas42@gmail.com>
2020-01-14 15:50:57 +0200

    Implemented an initial fine-grain locking LL.

--- ex3/z3/ll_fgl.c
+++ ex3/z3/ll_fgl.c
@@ -9,7 +9,7 @@
 typedef struct ll_node {
 	int key;
 	struct ll_node *next;
-	/* other fields here? */
+	pthread_spinlock_t *lock;
 } ll_node_t;
 
 struct linked_list {
@@ -27,6 +27,8 @@ static ll_node_t *ll_node_new(int key)
 	XMALLOC(ret, 1);
 	ret->key = key;
 	ret->next = NULL;
+	XMALLOC(ret->lock, 1);  //Clang-Tidy gives warning about this
+	pthread_spin_init(ret->lock, PTHREAD_PROCESS_PRIVATE);
 	/* Other initializations here? */
 
 	return ret;
@@ -37,6 +39,8 @@ static ll_node_t *ll_node_new(int key)
  **/
 static void ll_node_free(ll_node_t *ll_node)
 {
+    pthread_spin_destroy(ll_node->lock);
+    XFREE(ll_node->lock);
 	XFREE(ll_node);
 }
 
@@ -69,19 +73,93 @@ void ll_free(ll_t *ll)
 	XFREE(ll);
 }
 
+/* Works exactly like ll_remove(), locking the candidate node.
+ * I don't think any other node needs to be locked since in order
+ * for the ->next field to change, ll_add() and ll_remove() would
+ * need to lock the curr node first.
+*/
 int ll_contains(ll_t *ll, int key)
 {
-	return 0;
+    ll_node_t *curr;
+    pthread_spin_lock(ll->head->lock);
+    curr = ll->head;
+
+    while(curr->key < key)
+    {
+        pthread_spin_unlock(curr->lock);
+        curr = curr->next;
+        pthread_spin_lock(curr->lock);
+    }
+
+    if(curr->key == key)
+    {
+        pthread_spin_unlock(curr->lock);
+        return  1;
+    }
+    else
+    {
+        pthread_spin_unlock(curr->lock);
+        return 0;
+    }
 }
 
+// Works exactly like ll_remove(), but locking the candidate predecessor and successor
+// of the new node.
 int ll_add(ll_t *ll, int key)
 {
-	return 0;
+    ll_node_t *pred, *succ;
+    pthread_spin_lock(ll->head->lock);
+    pred = ll->head;
+    succ = pred->next;
+    pthread_spin_lock(succ->lock);
+
+    while(succ->key < key)
+    {
+        pthread_spin_unlock(pred->lock);
+        pred = succ;
+        succ = succ->next;
+        pthread_spin_lock(succ->lock);
+    }
+
+    // Create and insert new ll_node
+    ll_node_t  *new_node = ll_node_new(key);
+    pred->next = new_node;
+    new_node->next = succ;
+
+    pthread_spin_unlock(succ->lock);
+    pthread_spin_unlock(pred->lock);
+    return 1;
 }
 
+// Copied from lecture slides
 int ll_remove(ll_t *ll, int key)
 {
-	return 0;
+    ll_node_t *pred, *curr;
+    pthread_spin_lock(ll->head->lock);
+    pred = ll->head;
+    curr = pred->next;
+    pthread_spin_lock(curr->lock);
+
+    while(curr->key < key)
+    {
+        pthread_spin_unlock(pred->lock);
+        pred = curr;
+        curr = curr->next;
+        pthread_spin_lock(curr->lock);
+    }
+
+    if(curr->key == key)
+    {
+        pred->next = curr->next;
+        ll_node_free(curr);
+        pthread_spin_unlock(pred->lock);
+        return 1;
+    } else
+    {
+        pthread_spin_unlock(curr->lock);
+        pthread_spin_unlock(pred->lock);
+        return 0;
+    }
 }
 
 /**
